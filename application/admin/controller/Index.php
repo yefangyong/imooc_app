a7edfe8a2bc5e6f066dca27312fdbecdc7127ab3
yefy <1281074511@qq.com>
2017-11-26 15:48:55 +0800

    first commit

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\admin\controller;
+
+use think\Controller;
+
+class Index extends Controller
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+    public function welcome() {
+        echo 'hello world';
+    }
+}
